c1672199f5bc5cffceb56b8142377d0ab67470ad
Arjun Nair <nair.arjun47@yahoo.com>
2020-07-01 00:05:15 -0700

    Add java code for question 4 and change styling for theme.

--- app/src/main/java/com/example/romanimillennium/MainActivity.java
+++ app/src/main/java/com/example/romanimillennium/MainActivity.java
@@ -25,17 +25,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void correct(View view) {
-//        if (score == 0 ) {
-//            Toast.makeText(this, "Point!", Toast.LENGTH_LONG).show();
-//            return;
-//        }
-//        score = score + 1;
-//        displayScore (score);
-//
-//        RadioGroup group = (RadioGroup) findViewById(R.id.questionOne);
-//        group.setEnabled(false);
-//    }
 
     private void displayScore(int totalScore) {
         TextView score = (TextView) findViewById(R.id.score);
@@ -63,15 +52,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-
-//        RadioButton qOne = (RadioButton) findViewById(R.id.q1_b);
-//        boolean qOneAnswer = qOne.isChecked();
-//
-//        if (qOneAnswer) {
-//            score = score + 1;
-//        }
-//
-//        return  score;
     }
 
     public void questionTwoAns(View view) {
@@ -88,54 +68,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
-
-//    public void questionThreeAnsOne(View view) {
-//        CheckBox julius = (CheckBox) findViewById(R.id.q3_a);
-//        boolean questionThreeA = julius.isChecked();
-//
-//        CheckBox pompey = (CheckBox) findViewById(R.id.q3_d);
-//        boolean questionThreeD = pompey.isChecked();
-//
-//        CheckBox crassus = (CheckBox) findViewById(R.id.q3_f);
-//        boolean questionThreeF = crassus.isChecked();
-//
-//        if (questionThreeA && questionThreeD && questionThreeF == true) {
-//            score++;
-//        }
-//    }
-
-//    public void questionThree(View view) {
-//        // Is the view now checked?
-//        boolean checked = ((CheckBox) view).isChecked();
-//
-//        // Check which checkbox was clicked
-//        switch(view.getId()) {
-//            case R.id.q3_a:
-//                if (checked)
-//                // Add 1
-//                    score ++;
-//            else
-//                // Nothing
-//                break;
-//            case R.id.q3_d:
-//                if (checked)
-//                // Add 1
-//                    score ++;
-//            else
-//                // Nothing
-//                break;
-//            case R.id.q3_f:
-//                if (checked)
-//                    // Add 1
-//                    score ++;
-////                    if (questionThree == 3) {
-////                        score ++;
-////                    }
-//                else
-//                // Nothing
-//                break;
-//        }
-//    }
 
     public void questionThree(View view) {
         CheckBox choiceOne = (CheckBox) findViewById(R.id.q3_a);
@@ -170,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         return score + (3 / point);
     }
 
-    private boolean questionThreeAns(){
+    public boolean questionThreeAns(){
         CheckBox choiceOne = (CheckBox) findViewById(R.id.q3_a);
 
         CheckBox choiceTwo = (CheckBox) findViewById(R.id.q3_d);
@@ -183,6 +115,15 @@ public class MainActivity extends AppCompatActivity {
 
         return false;
     }
+
+    String last = "Last Roman";
+
+    private boolean questionFourAns() {
+        EditText roman = (EditText)findViewById(R.id.q4_a);
+
+        return roman.getText().toString().equals(last);
+    }
+
 
     public void questionFiveAns(View view) {
 
@@ -200,44 +141,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void finalScore(View view) {
-        if (questionThreeAns()) {
-            score++;
+        if (questionThreeAns() && questionFourAns()) {
+            score += 2;
             TextView quizScore = (TextView) findViewById(R.id.score);
             quizScore.setText("FinalScore: " + score +"/5");
-        } else  {
+        } else if (questionThreeAns() || questionFourAns()){
+            score ++;
+            TextView quizScore = (TextView) findViewById(R.id.score);
+            quizScore.setText("FinalScore: " + score +"/5");
+        } else {
             TextView quizScore = (TextView) findViewById(R.id.score);
             quizScore.setText("FinalScore: " + score +"/5");
         }
     }
-//
-////    public void submit(int score) {
-////        TextView quizScore = (TextView) findViewById(R.id.score);
-////        quizScore.setText("" + score);
-////    }
-//
-//    public  void submit(View view) {
-////        is radio clicked
-//        boolean checked = ((RadioButton) view).isChecked();
-//
-////        Check which radio was clicked
-//        switch (view.getId()) {
-//            case R.id.q1_b:
-//                if (checked)
-//                    score++;
-//                TextView quizScore = (TextView) findViewById(R.id.score);
-//                quizScore.setText("" + score);
-//                break;
-//            case R.id.q1_a:
-//                if (checked)
-//                    // Nothing
-//                break;
-//        }
-//
-//
-//    }
-
-
-
-
 
 }
